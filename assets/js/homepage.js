6be7247ad01c25b4773badfda1ac5a01082249f6
Daniel Gross <danwgross@gmail.com>
2020-09-09 09:01:19 -0400

    6.2.4 handle form submission

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,22 @@
+const userFormEl = document.querySelector("#user-form");
+const nameInputEl = document.querySelector("#username");
+
+const formSubmitHandler = (event) => {
+  event.preventDefault();
+  const username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a GitHub username");
+  }
+};
+
 const getUserRepos = async (user) => {
   const response = await fetch(`https://api.github.com/users/${user}/repos`);
   const data = await response.json();
   console.log(data);
 };
 
-getUserRepos("DanielWGross");
+userFormEl.addEventListener("submit", formSubmitHandler);
