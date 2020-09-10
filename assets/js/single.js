e36513738c858e3d7a221fc2662a8f76498212a3
Daniel Gross <danwgross@gmail.com>
2020-09-10 07:48:37 -0400

    6.3.5 convert fetched data into dom elements

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,3 +1,5 @@
+const isEmpty = (arr) => !Array.isArray(arr) || arr.length === 0;
+
 const getRepoIssues = async (repo) => {
   const apiUrl = `https://api.github.com/repos/${repo}/issues?direction=asc`;
   const response = await fetch(apiUrl).catch(() =>
@@ -5,10 +7,37 @@ const getRepoIssues = async (repo) => {
   );
   if (response.ok) {
     const data = await response.json();
-    co;
+    displayIssues(data);
   } else {
     alert(`Error: ${response.statusText}`);
   }
 };
 
-getRepoIssues("facebook/react");
+const displayIssues = (issues) => {
+  const issuesContainerEl = document.querySelector("#issues-container");
+
+  if (isEmpty(issues)) {
+    issuesContainerEl.textContent = "This repo has no open issues!";
+    return;
+  }
+
+  issues.forEach((issue) => {
+    const issueEl = document.createElement("a");
+    issueEl.classList = "list-item flex-row justify-space-between align-center";
+    issueEl.setAttribute("href", issue.html_url);
+    issueEl.setAttribute("target", "_blank");
+
+    const titleEl = document.createElement("span");
+    titleEl.textContent = issue.title;
+    issueEl.appendChild(titleEl);
+
+    const typeEl = document.createElement("span");
+
+    typeEl.textContent = issue.pull_request ? "(Pull request)" : "(Issue)";
+
+    issueEl.appendChild(typeEl);
+    issuesContainerEl.appendChild(issueEl);
+  });
+};
+
+getRepoIssues("DanielWGross/run-buddy");
